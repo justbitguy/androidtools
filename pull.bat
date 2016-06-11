3953d5b3bd5b35dc30d2068a358749c0d6e1da5e
justbitguy <justbitguy@gmail.com>
2016-06-11 23:32:28 +0800

    add date.

---
+++ pull.bat
@@ -0,0 +1,19 @@
+set packageName=com.quick.gamebooster
+
+set year=%date:~2,2%
+set month=%date:~5,2%
+set day=%date:~8,2%
+set hour=%time:~0,2%
+set minute=%time:~3,2%
+set seconds=%time:~6,2%
+
+echo day:%day%
+echo hour:%hour%
+set outDir=gb_%year%%month%%day%_%hour%%minute%
+echo %outDir%
+
+adb shell "run-as %packageName% cp -r /data/data/%packageName%" /sdcard/%outDir%
+adb pull /sdcard/%outDir%  %outDir%
+adb shell rm -rf /sdcard/%outDir%
+
+pause
--- pulldb.bat
+++
@@ -1,8 +0,0 @@
-set packageName=com.quick.gamebooster
-set dbName=info
-set dbOutputName=info.out
-
-adb shell "run-as %packageName% cp /data/data/%packageName%/databases/%dbName%" /sdcard/%dbOutputName%
-adb pull /sdcard/%dbOutputName%
-
-pause
