491495c2848badf09d84e500df559d1b2d5dc0de
justbitguy <justbitguy@gmail.com>
2016-06-13 01:24:36 +0800

    modify log file name by time format.

--- monkey.bat
+++ monkey.bat
@@ -6,38 +6,36 @@ set eventTime=500
 set eventCount=100
 
 :: 根据日期时间设置monkey的log目录
-set year=%date:~2,2%
-set month=%date:~5,2%
-set day=%date:~8,2%
 set hour=%time:~0,2%
-set minute=%time:~3,2%
-set logDir=monkey_%year%%month%%day%_%hour%%minute%
-set logFile=monkey_%year%%month%%day%_%hour%%minute%.txt
+if "%hour:~0,1%" == " " set hour=0%hour:~1,1%
 
+set min=%time:~3,2%
+if "%min:~0,1%" == " " set min=0%min:~1,1%
+
+set secs=%time:~6,2%
+if "%secs:~0,1%" == " " set secs=0%secs:~1,1%
+
+set year=%date:~2, 2%
+
+set month=%date:~5,2%
+if "%month:~0,1%" == " " set month=0%month:~1,1%
+
+set day=%date:~8,2%
+if "%day:~0,1%" == " " set day=0%day:~1,1%
+
+set logFile=monkey_%year%%month%%day%_%hour%%min%.txt
+ 
 :: 开始执行monkey命令
-set cmd="monkey -p %packageName% --pct-touch 40 --pct-motion 10 --pct-appswitch 40 --pct-anyevent 10 --throttle %eventTime% --ignore-crashes --ignore-timeouts --ignore-security-exceptions -v %eventCount%"
+set cmd="monkey -p %packageName% --pct-touch 40 --pct-motion 10 --pct-appswitch 40 --pct-anyevent 10 --throttle %eventTime% --ignore-crashes --ignore-timeouts --ignore-security-exceptions --script-log -v %eventCount%"
 echo %cmd%
 adb shell %cmd% > %logFile%
-
-:: 如果/sdcard目录下有crash文件，将文件保存到logDir, pull到本地后，删除/sdcard/logDir
-rem adb shell "cd /sdcard  && mkdir %logDir% && mv app_crash* %logDir%"
-rem adb pull /sdcard/%logDir% %logDir%
-rem adb shell "cd /sdcard && rm -rf %logDir%"
-
-rem :: 提示是存在崩溃
-rem echo "Finish Monkey Test!"
-rem @if exist %logDir% (
-rem 	echo "There are crashes!"
-rem ) else (
-rem 	echo "No crashes!"
-rem ) 
 
 findstr "CRASH:" %logFile% > crash.txt
 for /f %%i in ("crash.txt") do set size=%%~zi
 @if %size% gtr 0 (
-	echo Has Crash!
+	echo There are crashes!
 ) else (
-	echo  No Crash!
+	echo There are no crashes!
 )
 
 pause
